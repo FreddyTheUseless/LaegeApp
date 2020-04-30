6e3c7d3344437b41941976fce5706c33b5aba8fb
Gustav <gusw.petersen@gmail.com>
2020-04-30 14:18:01 +0200

    Update database_handler.dart

--- laege_app/lib/helpers/database_handler.dart
+++ laege_app/lib/helpers/database_handler.dart
@@ -1,3 +1,17 @@
+// * Packages
+import 'package:http/http.dart' as http;
+
 class DataBaseHandler {
-  
+  Future<void> doSomething() async {
+
+  }
+  Future<void> doSomethingElse() async {
+    
+  }
+  Future<void> doThis() async {
+    
+  }
+  Future<void> doThat() async {
+    
+  }
 }
