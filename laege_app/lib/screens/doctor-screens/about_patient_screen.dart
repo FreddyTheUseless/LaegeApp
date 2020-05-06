da8411331724654b648dc808f33696a03df4e4b2
Gustav <gusw.petersen@gmail.com>
2020-05-06 13:44:52 +0200

    added About patient screen

---
+++ laege_app/lib/screens/doctor-screens/about_patient_screen.dart
@@ -0,0 +1,124 @@
+// * Packages
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:laege_app/components/doctor_components/request_meeting_button.dart';
+import 'package:laege_app/components/doctor_components/top_doctor_name.dart';
+import 'package:laege_app/components/launch_screen_components/top_app_title.dart';
+
+class AboutPatientScreen extends StatelessWidget {
+  AboutPatientScreen({
+    this.patientName,
+  });
+
+  final String patientName;
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+        width: screenSize.width,
+        height: screenSize.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TopAppTitle(),
+            TopDoctorName(doctorName: patientName),
+            SizedBox(height: screenSize.height * 0.1),
+            RequestMeetingButton(screenSize: screenSize),
+            SizedBox(height: screenSize.height * 0.1),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Aftaler',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF6F909F),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: screenSize.height * 0.02),
+            AppointmentWidget(
+              screenSize: screenSize,
+              appointmentTitle: 'Corona Tjek',
+              date: '15:10 - 6. Maj / 2020',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppointmentWidget extends StatelessWidget {
+  const AppointmentWidget({
+    @required this.screenSize,
+    this.appointmentTitle,
+    this.date,
+  });
+
+  final Size screenSize;
+  final String appointmentTitle;
+  final String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+      width: screenSize.width,
+      height: screenSize.height * 0.075,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 6.0,
+            color: Colors.black12,
+            offset: Offset(0, 3),
+            spreadRadius: 2.0,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                appointmentTitle,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF808080),
+                ),
+              ),
+              Text(
+                date,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFAFAFAF),
+                ),
+              ),
+            ],
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Icon(
+              CupertinoIcons.video_camera_solid,
+              size: 50,
+              color: Color(0xFF6F909F),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- laege_app/lib/screens/doctor-screens/doctor_home_screen.dart
+++ laege_app/lib/screens/doctor-screens/doctor_home_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:laege_app/components/launch_screen_components/top_app_title.dart';
 import 'package:laege_app/components/main_screen_components/appointment_title.dart';
 import 'package:laege_app/components/main_screen_components/doctor_appointment.dart';
+// * Screens
+import './about_patient_screen.dart';
 
 class DoctorHomeScreen extends StatelessWidget {
   static const routeName = '/doctor-home-screen';
@@ -29,7 +31,9 @@ class DoctorHomeScreen extends StatelessWidget {
               imageUrl:
                   'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png',
               name: 'Gustav Walker Petersen',
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => AboutPatientScreen(patientName: 'Gustav Walker Petersen',)));
+              },
             ),
             SizedBox(height: screenSize.height * 0.03),
             DoctorAppointment(
@@ -37,7 +41,9 @@ class DoctorHomeScreen extends StatelessWidget {
               imageUrl:
                   'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png',
               name: 'Fake Fakesen',
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => AboutPatientScreen(patientName: 'Fake Fakesen',)));
+              },
             ),
             SizedBox(height: screenSize.height * 0.03),
             DoctorAppointment(
@@ -45,14 +51,18 @@ class DoctorHomeScreen extends StatelessWidget {
               imageUrl:
                   'https://soundvenue.com/wp-content/uploads/2016/09/Daniel-Craig-James-Bond-640x640.jpg',
               name: 'James Bond',
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => AboutPatientScreen(patientName: 'James Bond',)));
+              },
             ),
             SizedBox(height: screenSize.height * 0.03),
             DoctorAppointment(
               screenSize: screenSize,
               imageUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/9/97/Harry_Potter.jpg/revision/latest?cb=20150811193929&path-prefix=da',
               name: 'Harry James Potter',
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => AboutPatientScreen(patientName: 'Harry James Potter',)));
+              },
             ),
           ],
         ),
