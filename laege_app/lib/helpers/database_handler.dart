baf9bc337aa7518c6e87ed189e3e3aaf12f47ca2
Gustav <gusw.petersen@gmail.com>
2020-05-07 16:51:07 +0200

    gg wp

--- laege_app/lib/helpers/database_handler.dart
+++
@@ -1,9 +0,0 @@
-// * Packages
-import 'package:http/http.dart' as http;
-
-class DataBaseHandler {
-  Future<void> sendToDatabase() async {}
-  Future<void> retrieveFromDatabase() async {}
-  Future<void> updateDatabase() async {}
-  Future<void> deleteFromDatabase() async {}
-}
